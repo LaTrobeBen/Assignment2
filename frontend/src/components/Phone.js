fb9efd68a7a17a4dee23de7e8663c95471134e74
Benjamin Smerd <bjsmerd@gmail.com>
2025-08-31 16:43:29 +0930

    update frontend for the phoine component to show the phoneType and phoneNumber

--- frontend/src/components/Phone.js
+++ frontend/src/components/Phone.js
@@ -15,8 +15,8 @@ function Phone(props) {
 
 	return (
 		<tr>
-            <td>{ phone.name }</td>
-            <td>{ phone.number }</td>
+            <td>{ phone.phoneType }</td>
+            <td>{ phone.phoneNumber }</td>
             <td style={
                 {
                     width: '14px',
